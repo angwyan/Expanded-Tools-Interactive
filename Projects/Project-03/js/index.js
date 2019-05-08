99816dbdaf47a4c978e902cc9ffb84d0895a3e06
app <app@appdeMacBook-Pro.local>
2019-05-08 15:20:13 -0700

    update

---
+++ Projects/Project-03/js/index.js
@@ -0,0 +1,97 @@
+var Panda_json = [{
+		title: "Panda's introduction",
+		content: "&nbsp;&nbsp;Panda, is a carnivorous mammal belongs in the bear family, is very fat, similar to a bear, but its head is round and large, its tail is short, and the skin of the head and body is black and white. In addition to the five toes, the forefoot also has a sixth toe. Panda is a unique species in China, and the main existing habitat is the mountainous area around the Sichuan Basin in China. There are about 1,590 wild pandas in the world, which are endangered species in the Chinese animal class and are China's national treasure.",
+		img_name: "ct_2.jpg",
+		video_name: "Panda_4.mp4"
+	},
+	{
+		title: "Panda's historical development",
+		content: "&nbsp;&nbsp;Panda has lived in the world when human evolution has not yet been completed. According to the analysis of the panda fossils that have been discovered, as early as 8 million years ago, on the edge of tropical humid forests in Yunnan, China, the ancestor of the panda—— Ailurarctos Lufengensis lived here. This is the earliest carnivorous panda evolved from the pseudo-bears, and the individual is like a fatter fox. Through the fossil study of the Ailurarctos panda, it was found to be an omnivorous animal and very ferocious. According to Chinese ancient books, more than 4,000 years ago, the Huang Di led his army to defeat Yan Di's army. The secret of winning was because they used domesticated tigers, leopards, bears, pandas and other beasts. In ancient books more than 3,000 years ago, it was clearly recorded that people at that time wanted the brave to be as brave as a panda. Even the army’s flag was held with a panda pattern to encourage the soldiers. Later, the book《Shan Hai Jing》recorded the pandas more and more strangely. They thought that the pandas had sharp teeth and could chew copper and iron, so people called them 'iron beasts'. In fact, in the absence of salt, the panda will break into the villagers home to eat the salt remaining in the iron pot. The villagers did not understand the panda's habits and mistakenly thought that the pandas were eating iron, so the panda got the name 'iron beasts'.Therefore, the habitus of the panda to eat bamboo is not natural but acquired. The dense bamboo forest is the natural barrier of the pandas to avoid tigers, leopards, and other beasts. This is the secret of the panda's 'hidden' bamboo forest. From the time of the Ailurarctos pandas, they will be hidden in the bamboo forest whenever they are in danger. The panda further adapted to the life of the subtropical bamboo forest and eventually evolved into a living habit of eating bamboo. So the power of natural selection led the panda to eat bamboo. As they struggle for survival, from omnivorous to vegetarian, their character has become mild.Because bamboo is difficult to digest, pandas can only suck 17% of the nutrients from the bamboo. In order to survive, the panda needs to eat bamboo constantly. But the bamboo has a high sugar content, which causes the panda to gain weight. Panda's fat body is not only because it eats too much, but more importantly, animals can only rely on their own fur to withstand the cold, and the panda skin is very thick, up to 10 mm thick, and the fur is elastic and tough. In the harsh natural environment, large pandas are better able to endure the cold weather. Pandas are slow-moving, consume a lot of energy, and easily adapt to frequent changes and extreme natural environments. And the black and white appearance is conducive to hiding in the trees of the forest and the snowy ground, not easy to be discovered by natural enemies.It can be found that each time the panda evolves, it is better to adapt to the natural environment and protect itself. So the panda experienced a magical evolution from 8 million years ago to the present, and still retains strong evolutionary potential. So in the evolutionary history of animals and competition for survival, pandas are successful.Reasons for a large reduction in pandas：However, due to the blind activities of human beings, their living environment has been destroyed, resulting in a large reduction in their habitat area. Because the panda's food source is single, it relies entirely on bamboo for a living. Once the bamboo bloomed in a large area, the number of pandas decreased significantly. And the panda has the poor reproductive ability, and the survival rate of the panda baby is low. It is also an important thing that a large number of pursuers have come to China to arrest pandas. A large number of pandas died on the road to smuggling. The number of pandas has dropped dramatically and is almost extinct.）",
+		img_name: "ct_6.jpg",
+		video_name: "Panda_2.mp4"
+	},
+	{
+		title: "Panda distribution",
+		content: "'&nbsp;&nbsp;Panda Guardian' is a public welfare action organized by Weibo in cooperation with the China Green Foundation. The activity sends a video of the panda and knowledge about the panda on Weibo every day. And weibo users also can helping the panda to raise their virtual bamboo in the 'Panda Guardian'. And they can learn about the panda protection knowledge through the Panda Guardian. Then based on the number of virtual bamboos and the actual natural conditions of the panda habitat,the sponsors planted real bamboo in the panda habitat to scientifically repair the bamboo forest ecology of the panda habitat and alleviate the problem of “fragmentation” of the panda habitat. Local workers were hired for planting and raising work, which solved many local people's work problems. The lives of local residents have been closely linked to the protection of pandas.<br>  The 'Panda Guardian' quickly became popular with users. The Panda Guardian campaign took less than 6 months and the fans broke through 10 million. The product went online less than 9 months, the user broke 20 million, and invited many stars to promotion about protect the panda. The purpose of the 'Panda Guardian' is to enhance the public's understanding of panda protection through Weibo transmission and social attributes.<br>  'Panda Guardian' also opened the 'Panda Postcard' function, the postcard has pictures of wild animals being captured, and has relevant scientific knowledge. In the subtle way, let users know the living status of wild species and cultivate the awareness of animal protection of users. And an important purpose is to help protect the ground to set up more infrared cameras, conduct wildlife behavior monitoring, obtain wildlife image data, and provide important information for wildlife protection.）<br>",
+		img_name: "ct_3_1.gif",
+		video_name: "Panda_4.mp4"
+	},
+	{
+		title: "Panda protection",
+		content: "After the founding of New China, pandas were valued and protected in China. Then, with the increasing scientific and specific protection measures, the number of pandas has increased significantly. And it is regarded as a national treasure animal. As a signature animal of the Chinese image brand. Because the panda is a rare animal unique to China. In the natural environment rule and survival competition, most animals are extinct, but the panda has survived. Moreover, the mild character represents China’s willingness to yearn for peace. The idea of ​​'peace' in China is reflected in the black and white fur of the pandas. Because of the combination of black and white, it represents an important meaning in Chinese philosophy. For example, the Chinese Taiji, black and white, symbolizes balance and matches with the traditional Chinese philosophy. It is more helpful to let the people of the world understand China.Because of these characteristics of the panda, it makes it represent the image of the country. It also gives the panda better protection. Including the establishment of the Panda Sanctuary. Create a website about panda information, such as the 'chinagiantpanda' website. The website comprehensively introduces the protection of pandas, the main Panda Sanctuary and the Panda Conservation Research Institute, which records the location and time of the pandas found in the wild and how to protect the pandas. It also focused on the field tracking of panda activities and panda volunteer activities. There are also have Panda live video, Panda Games, etc. Many local companies and brands are named after the panda. Includes logo, including products. Even the primary school's annual cultural performances will have a program to play pandas. Because of the extensive publicity of pandas that people will fully understand and like pandas. There are also more and more volunteers who are willing to participate in the observation of pandas and the protection of pandas. It can be seen that the panda can obtain such successful protection, except that because it is a rare animal, one of the important reasons is that the panda represents the image of China and the local.Panda data collection<br>eMammal is a system for collecting, storing and sharing camera trap data. The system is designed for scientists and citizen scientists and anyone who wants to join the camera trap for fun and discovery. Citizen volunteers place 'camera traps', infrared activated cameras in the landscape of parks and other natural areas to collect photos of mammals. Camera captures then use our custom software to view images, identify animals, and upload them to a data repository for viewing and storage. These data help researchers and scientists study and protect animals better. “eMammal is at the cutting edge intersection of the new technologies surrounding cloud computing and “big data” and the time honored tradition of citizens volunteering to help conservation efforts,” said Smithsonian Conservation Biology Institute Conservation Biologist, Tavis Forrester, Ph.D.The Panda Project aims to determine the distribution of pandas and other large mammal species in and around the Panda Nature Reserve and to help local protected areas establish a large panda monitoring system that uses camera trapping. These data help researchers answer questions about the regional distribution and latitude of pandas and use this information to protect pandas. They also provide a unique perspective on the hidden wildlife world.<br> https://emammal.si.edu/search/node/panda<br>This citizenship program, all ages, education levels and background groups can participate in volunteers, to guide users to participate more deeply in the scientific protection of pandas and panda habitats. Let more people pay attention to wild pandas and gather more social power for the protection of wild pandas. Let the public subconsciously accept panda protection knowledge, further arousing the awareness of users to pay attention to ecology and protect the panda.",
+		img_name: "eMammal_ct.png",
+		video_name: "Panda_3.mp4"
+	},
+	{
+		title: "The future development direction of rare animals",
+		content: " &nbsp;&nbsp;In order to better protect the panda, the government must protect the forest and provide a range of services for people living in and around the reserve. For example, forests make it suitable for growing crops and stocking livestock, storing and cleaning fresh water, providing firewood and many useful plants. They help store carbon and help prevent soil erosion. These measures have greatly improved the living conditions of local residents. Data show that from 2000 to 2010, the annual income of residents in the Panda Sanctuary increased by an average of 56%, and the annual income of farmers in the surrounding Panda Sanctuary increased by an average of 64%. The protection measures for pandas have changed the development model and economic structure of a region and also changed the fate of people in a region. Scientists involved in the study said that these protected areas will bring in the annual economic value of up to 6.9 billion US dollars, equivalent to 27.1 times the cost of protecting these habitats. It can be seen that human society and animals are an inseparable unity. While protecting animals, they also bring huge benefits to the development of the city.<br>  So if every animal can be given its own role, meaning, and identity, can animals get better protected? Different regions, or companies, brands find and use this animal that can be used to represent their concept. Then each region and brand can carry out extensive publicity, investment, and protection for their own animals. It will also let more people know and understand different animals, and will better protect them and enhance their value. It will also promote local and company visibility and development. Just as pandas are integrated into our social life, other animals are truly integrated into our social life and become an integral part of society. In this way, they can be better protected, and at the same time bring benefits to the development of society and the environment.<br>  This is a new way of thinking. We no longer protect animals just for animals, but we clearly want to help humans by protecting animals. This is not entirely for human nature, because it is not just a question of ecosystems providing direct products to humans. Instead, we need to see human society and the wild ecosystem as an inseparable whole. Metz called this idea 'natural and human'. Ecologists have accumulated a lot of evidence that the more species they have, the more stable the ecosystem and the greater the resilience; on the contrary, the less species the ecosystem, the more likely it is to collapse. Human society is also part of the ecosystem, and a stable ecosystem will serve us. So living in harmony with nature is our only choice. This does not mean that we have to protect every species because we cannot do it. But this is not meant to keep everything the same as with the original state. The real significance of this concept is that we must maintain the richness and diversity of the ecosystem as much as possible, which is beneficial not only to nature but also to human beings<br>",
+		img_name: "ct_5.jpg",
+		video_name: "Panda_3.mp4"
+	}
+];
+
+
+
+function Eject(index) {
+
+	if(index==2){
+		var She_content3=document.getElementById("She_content3");
+		She_content3.style.display="block";
+		return ;
+	}
+	if(index==3){
+		var She_content2=document.getElementById("She_content2");
+		She_content2.style.display="block";
+		return ;
+	}
+	
+	var She_content = document.getElementById("She_content");
+	var Video = document.getElementById("Video");
+	var title = document.getElementById("title");
+	var content_text = document.getElementById("content_text");
+	var content_img = document.getElementById("content_img");
+
+	title.innerHTML = Panda_json[index].title;
+	content_img.src = "images/ct/" + Panda_json[index].img_name;
+	content_text.innerHTML = Panda_json[index].content;
+	Video.src = "vadeo/"+Panda_json[index].video_name;
+	She_content.style.display = "block";
+	if(index==4){
+		content_img.style.display="none";
+	}else{
+		content_img.style.display="block";
+	}
+	if(index==1){
+		Video.style.display="none";
+	}else{
+		Video.style.display="block"
+		Video.play();
+	}
+	
+}
+function Close2() {
+	var She_content2=document.getElementById("She_content2");
+	She_content2.style.display="none";
+}
+function Close(id) {
+	if(id==2){
+		var She_content = document.getElementById("She_content2");
+		var Video1 = document.getElementById("Video1");
+//		var Video2 = document.getElementById("Video2");
+		She_content.style.display = "none";
+		Video1.pause();
+//		Video2.pause();
+		return ;
+	}else if(id==3){
+		var She_content = document.getElementById("She_content3");
+		var Video3 = document.getElementById("Video3");
+		She_content.style.display = "none";
+		Video3.pause();
+		return ;
+	}
+		var She_content = document.getElementById("She_content");
+		She_content.style.display = "none";
+
+		Video.pause();
+	
+}
